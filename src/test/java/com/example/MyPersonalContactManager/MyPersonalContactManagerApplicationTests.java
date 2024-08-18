52936ba1c7166ec21495a87002ea21f1b69c06b1
Natalia <kolchenko.nata.yu@gmail.com>
2024-08-18 20:21:36 +0200

    create "main"

---
+++ src/test/java/com/example/MyPersonalContactManager/MyPersonalContactManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyPersonalContactManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersonalContactManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
