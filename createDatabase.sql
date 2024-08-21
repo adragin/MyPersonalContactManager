a7d471fef56aa7bf37cb0180c24f4787407759ca
Natalia Kolchenko <160338391+NataliaKolchenko@users.noreply.github.com>
2024-08-21 13:53:46 +0200

    ScriptCreateDatabase

---
+++ createDatabase.sql
@@ -0,0 +1,33 @@
+-- CREATE DATABASE MyPersonalCpntactManager_DB;
+
+-- USE MyPersonalCpntactManager_DB;
+
+-- CREATE TABLE Contacts (
+--     id varchar(36),
+--     First_Name VARCHAR(128) NOT NULL,
+--     Last_Name VARCHAR(128),
+--     Email varchar (50),
+--     Phone varchar(15),
+--     Birth_Day DATE,
+--     Address VARCHAR(128),
+--     Photo varchar(100),
+--     Create_Date TIMESTAMP
+-- );
+
+-- select * from Contacts;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
