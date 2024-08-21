21f1e5343d0844b8de93de0b980231ce8bec5960
Aleksandr <dragin.alex@gmail.com>
2024-08-21 18:37:22 +0200

    start 10 contacts.sql added

---
+++ Project_Documentations/start_10_contacts.sql
@@ -0,0 +1,12 @@
+INSERT INTO Contacts (id, First_Name, Last_Name, Email, Phone, Birth_Day, Address, Photo, Create_Date)
+VALUES
+  ('1', 'John', 'Doe', 'johndoe@example.com', '+12125551212', '1980-01-01', 'New York, NY', 'avatar1.jpg', NOW()),
+  ('2', 'Jane', 'Smith', 'janesmith@example.com', '+14155551212', '1985-05-15', 'San Francisco, CA', 'avatar2.jpg', NOW()),
+  ('3', 'Michael', 'Johnson', 'michaeljohnson@example.com', '+13125551212', '1990-11-20', 'Chicago, IL', 'avatar3.jpg', NOW()),
+  ('4', 'David', 'Williams', 'davidwilliams@example.com', '+12025551212', '1995-07-05', 'Washington, DC', 'avatar4.jpg', NOW()),
+  ('5', 'Emily', 'Brown', 'emilybrown@example.com', '+16175551212', '1992-03-10', 'Boston, MA', 'avatar5.jpg', NOW()),
+  ('6', 'Daniel', 'Anderson', 'danielanderson@example.com', '+13105551212', '1988-09-25', 'Los Angeles, CA', 'avatar6.jpg', NOW()),
+  ('7', 'Olivia', 'Taylor', 'oliviataylor@example.com', '+17135551212', '1975-12-31', 'Houston, TX', 'avatar7.jpg', NOW()),
+  ('8', 'James', 'White', 'jameswhite@example.com', '+14045551212', '1998-02-15', 'Atlanta, GA', 'avatar8.jpg', NOW()),
+  ('9', 'Sophia', 'Harris', 'sophiaharris@example.com', '+12135551212', '1983-06-20', 'Los Angeles, CA', 'avatar9.jpg', NOW()),
+  ('10', 'Benjamin', 'Nelson', 'benjaminnelson@example.com', '+17735551212', '1978-08-05', 'Chicago, IL', 'avatar10.jpg', NOW());
