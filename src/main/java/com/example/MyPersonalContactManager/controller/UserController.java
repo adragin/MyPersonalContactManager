ed7bc01fb4a30a78f93d5d1f8f19a42f2e9e7341
Aleksandr <dragin.alex@gmail.com>
2024-08-24 22:36:53 +0200

    InterfaceUserRepo + InterfaceUserService + мелкие правки USER моделей

--- src/main/java/com/example/MyPersonalContactManager/controller/UserController.java
+++ src/main/java/com/example/MyPersonalContactManager/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.MyPersonalContactManager.controller;
 import com.example.MyPersonalContactManager.models.UserModels.UserDTOLogin;
 import com.example.MyPersonalContactManager.models.UserModels.UserDTORegister;
 import com.example.MyPersonalContactManager.models.UserModels.UserDTOResponse;
+import com.example.MyPersonalContactManager.service.InterfaceUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,16 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
 
-    @PostMapping (value = "/userRegistration")
-    public ResponseEntity<UserDTOResponse> userRegistration (UserDTORegister userDto) {
+    public final InterfaceUserService dbUserService;
+
+    @PostMapping(value = "/userRegistration")
+    public ResponseEntity<UserDTOResponse> userRegistration(UserDTORegister userDto) {
         return null;
     }
 
     @GetMapping("/users/authUser")
-    public ResponseEntity<UserDTOResponse> userRegistration (UserDTOLogin userDto) {
+    public ResponseEntity<UserDTOResponse> userRegistration(UserDTOLogin userDto) {
         return null;
     }
+
+
 }
--- src/main/java/com/example/MyPersonalContactManager/models/UserModels/UserToken.java
+++ src/main/java/com/example/MyPersonalContactManager/models/UserModels/UserToken.java
@@ -9,8 +9,8 @@ import java.util.UUID;
 
 @Setter
 @Getter
-public class UserToken extends User{
-   private final String token;
+public class UserToken extends User {
+    private final String token;
     private UUID userId;
     private LocalDateTime createDate;
     private LocalDateTime lastUpdateDate;
@@ -19,8 +19,8 @@ public class UserToken extends User{
     public UserToken() {
         this.token = authUser.generateToken(getLogin(), getPassword());
         this.userId = getUserId();
-        this.createDate = LocalDateTime.now();;
-        this.lastUpdateDate = LocalDateTime.now();;
+        this.createDate = LocalDateTime.now();
+        this.lastUpdateDate = LocalDateTime.now();
     }
 }
 
